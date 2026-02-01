b8e2730bbcc05dc8b2f36b5e42b6e09b2781cf63
Jane <yevheniiasimaka@gmail.com>
2026-02-01 15:07:13 -0700

    added coins and dying

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,7 @@
 extends Area2D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var game_manager: Node = $"../GameManager"
 
 
-func _on_body_entered(_body: Node2D) -> void:
-	game_manager.add_point()
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 coin")
 	queue_free()
