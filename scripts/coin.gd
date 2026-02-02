14aea7d7f8baa3fb7bab8fb0caf9210d3842dd7d
Jane <yevheniiasimaka@gmail.com>
2026-02-01 19:44:16 -0700

    added score tracking

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 
 func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
 	print("+1 coin")
 	queue_free()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,12 +2,8 @@ extends Node
 
 var score = 0
 @onready var score_label: Label = $ScoreLabel
-@onready var game_manager: Node = $"."
+
 
 func add_point():
 	score += 1
-	print("hello" + score)
 	score_label.text = "You collected " + str(score) + " coins."
-	
-	if score == 90:
-		print("won")
